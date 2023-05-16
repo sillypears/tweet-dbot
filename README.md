1ee50a7b49199c5c812079c76f7ae5adc2880cfe
sillypears <sillypears@users.noreply.github.com>
2023-05-16 09:01:16 +0000

    made it better

--- README.md
+++ README.md
@@ -10,8 +10,8 @@
 
 docker build --tag tweet_dbot .
 docker run tweet_dbot
-docker tag tweet_dbot:latest tweet_dbot:v1.1
+docker tag tweet_dbot:latest tweet_dbot:v1.2
 docker login
 docker tag tweet_dbot:latest <userid>/tweet_dbot:latest
 docker push <userid>/tweet_dbot
-docker run -d -e DISCORD_TOKEN="$DISCORD_TOKEN" -e CLIENT_ID=$CLIENT_ID -e GUILD_ID=$GUILD_ID TWEET_CHANNEL_ID=$TWEET_CHANNEL_ID sillypairs/tweet_dbot:latest
+docker run -d -e DISCORD_TOKEN="$DISCORD_TOKEN" -e CLIENT_ID=$CLIENT_ID -e GUILD_ID=$GUILD_ID -e TWEET_CHANNEL_ID=$TWEET_CHANNEL_ID -e CHECK_URL=$CHECK_URL sillypairs/tweet_dbot:latest
--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from config import Config
 conf = Config(os.environ.get('DISCORD_TOKEN', 'failure'),os.environ.get('CLIENT_ID', 'failure'),os.environ.get('GUILD_ID', 'failure'), os.environ.get('TWEET_CHANNEL_ID', 'failure'))
 MY_GUILD = discord.Object(id=conf.GUILD_ID)
 TWEET_CHANNEL = discord.Object(id=conf.TWEET_CHANNEL_ID)
+CHECK_URL = os.environ.get('CHECK_URL')
 
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
@@ -55,7 +56,7 @@ async def clear_tweets(interaction: discord.Interaction ):
     """ Clear Tweets lol """
     
     try:
-        res = requests.post('http://192.168.1.55:4999/clearNewTweets')
+        res = requests.post(f"{CHECK_URL}/clearNewTweets")
         if res.status_code == 200:
             await interaction.response.send_message(f"Cleared {res.json()['cleared']} tweets!");
         else:
@@ -63,13 +64,11 @@ async def clear_tweets(interaction: discord.Interaction ):
     except Exception as e:
         await interaction.response.send_message(f"Yo shit's broken: {e}");
 
-    # await interaction.response.send_message("Hello!")
-
 @client.tree.context_menu(name='Clear Tweets')
 @discord.app_commands.describe(text_to_send='Clear them tweets')
 async def test(interaction: discord.Interaction, message: discord.Message):
     try:
-        res = requests.post('http://192.168.1.55:4999/clearNewTweets')
+        res = requests.post(f"{CHECK_URL}/clearNewTweets")
         if res.status_code == 200:
             await interaction.response.send_message(f"Cleared {res.json()['cleared']} tweets!");
         else:
@@ -80,11 +79,15 @@ async def test(interaction: discord.Interaction, message: discord.Message):
 @tasks.loop(minutes=5, count=None)
 async def get_tweet_count():
     await client.wait_until_ready()
-    res = requests.get('http://192.168.1.55:4999/getNewTweets')
+    res = requests.get(f"{CHECK_URL}/getNewTweets")
     if res.status_code == 200:
-        d = res.json()
-        channel = client.get_channel(TWEET_CHANNEL)
-        print(d, get_tweet_count.count, channel)
-        await channel.edit(topic=f"Tweets: {d['newTweets']}")
 
+        d = res.json()
+        try:
+            channel = (client.get_channel(TWEET_CHANNEL) or await client.fetch_channel(conf.TWEET_CHANNEL_ID))
+            if channel.topic != f"Tweets: {d['newTweets']}":
+                print(d, get_tweet_count.count, channel)
+                await channel.edit(topic=f"Tweets: {d['newTweets']}")
+        except Exception as e:
+            print(f"channel is: {e}")
 client.run(conf.DISCORD_TOKEN)
